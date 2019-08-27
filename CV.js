c76765e2fd4a86e4676f6884a1c1013c04ca60cc
ngrover <ngrover@mitrmedia.com>
2019-08-27 21:02:47 +0530

    Layout additions

--- CV.js
+++ CV.js
@@ -11,8 +11,8 @@ class resume {
       case 'loadJSON':
         this.loadJSON();
       break; 
-      case 'createElements':
-        this.createElements();
+      case 'createStructure':
+        this.createStructure();
       break;
     }
   }
@@ -27,86 +27,134 @@ class resume {
     .then(res => res.json())
     .then(response => {
       this.config = response;
-      this.loadSequence('createElements');
+      this.loadSequence('createStructure');
     })
     .catch(error => console.error('Error:', error));
   }
 
-  createElements() {
+  createStructure() {
     this.topNavigation();
     
     this.resumeContent();
   }
 
   topNavigation() {
-    this.createDiv({ className: 'topNavContainer', id: 'topNavContainer', appendTo: 'main' });
+    this.createElement({ className: 'topNavContainer', id: 'topNavContainer', appendTo: 'main' });
 
-    this.createDiv({ className: 'leftPartition', id: 'leftPartition', appendTo: 'topNavContainer' });
+    this.createElement({ className: 'leftPartition', id: 'leftPartition', appendTo: 'topNavContainer' });
     // left-block
-    this.createDiv({ className: 'leftBlock', id: 'leftBlock', appendTo: 'leftPartition' });
+    this.createElement({ className: 'leftBlock', id: 'leftBlock', appendTo: 'leftPartition' });
     // text-bloc
-    this.createDiv({ className: 'textBlock', id: 'textBlock', appendTo: 'leftPartition' });
+    this.createElement({ className: 'textBlock', id: 'textBlock', appendTo: 'leftPartition' });
     // name
-    this.createDiv({ className: 'name', id: 'name', appendTo: 'textBlock', html: this.config.elements.topNav.name });
+    this.createElement({ className: 'name', id: 'name', appendTo: 'textBlock', html: this.config.elements.topNav.name });
     // designation
-    this.createDiv({ className: 'designation', id: 'designation', appendTo: 'textBlock', html: this.config.elements.topNav.designation });
+    this.createElement({ className: 'designation', id: 'designation', appendTo: 'textBlock', html: this.config.elements.topNav.designation });
     // bio
-    this.createDiv({ className: 'bio', id: 'bio', appendTo: 'textBlock', html: this.config.elements.topNav.bio });
+    this.createElement({ className: 'bio', id: 'bio', appendTo: 'textBlock', html: this.config.elements.topNav.bio });
 
-    this.createDiv({ className: 'rightPartition', id: 'rightPartition', appendTo: 'topNavContainer' });
+    this.createElement({ className: 'rightPartition', id: 'rightPartition', appendTo: 'topNavContainer' });
 
     //icon holder
-    this.createDiv({ className: 'iconsHolder', id: 'iconsHolder', appendTo: 'rightPartition' });
+    this.createElement({ className: 'iconsHolder', id: 'iconsHolder', appendTo: 'rightPartition' });
 
 
     // message Icon
-    this.createDiv({ className: 'iconHolder', id: 'iconHolder1', appendTo: 'iconsHolder' });
-    this.createDiv({ className: 'iconText', id: 'email', appendTo: 'iconHolder1', html: this.config.elements.topNav.email });
-    this.createDiv({ className: 'icon', id: 'icon1', appendTo: 'iconHolder1' });
-    this.createDiv({ className: 'fas fa-envelope icon', id: 'email', appendTo: 'icon1' });
+    this.createElement({ className: 'iconHolder', id: 'iconHolder1', appendTo: 'iconsHolder' });
+    this.createElement({ className: 'iconText', id: 'email', appendTo: 'iconHolder1', html: this.config.elements.topNav.email });
+    this.createElement({ className: 'icon', id: 'icon1', appendTo: 'iconHolder1' });
+    this.createElement({ className: 'fas fa-envelope icon', id: 'email', appendTo: 'icon1' });
 
     // mobile Icon
-    this.createDiv({ className: 'iconHolder', id: 'iconHolder2', appendTo: 'iconsHolder' });
-    this.createDiv({ className: 'iconText', id: 'number', appendTo: 'iconHolder2', html: this.config.elements.topNav.number });
-    this.createDiv({ className: 'icon', id: 'icon2', appendTo: 'iconHolder2' });
-    this.createDiv({ className: 'fas fa-mobile-alt icon', id: 'number', appendTo: 'icon2' });
+    this.createElement({ className: 'iconHolder', id: 'iconHolder2', appendTo: 'iconsHolder' });
+    this.createElement({ className: 'iconText', id: 'number', appendTo: 'iconHolder2', html: this.config.elements.topNav.number });
+    this.createElement({ className: 'icon', id: 'icon2', appendTo: 'iconHolder2' });
+    this.createElement({ className: 'fas fa-mobile-alt icon', id: 'number', appendTo: 'icon2' });
 
     // location Icon
-    this.createDiv({ className: 'iconHolder', id: 'iconHolder3', appendTo: 'iconsHolder' });
-    this.createDiv({ className: 'iconText', id: 'location', appendTo: 'iconHolder3', html: this.config.elements.topNav.location });
-    this.createDiv({ className: 'icon', id: 'icon3', appendTo: 'iconHolder3' });
-    this.createDiv({ className: 'fas fa-map-marker-alt icon', id: 'location', appendTo: 'icon3' });
+    this.createElement({ className: 'iconHolder', id: 'iconHolder3', appendTo: 'iconsHolder' });
+    this.createElement({ className: 'iconText', id: 'location', appendTo: 'iconHolder3', html: this.config.elements.topNav.location });
+    this.createElement({ className: 'icon', id: 'icon3', appendTo: 'iconHolder3' });
+    this.createElement({ className: 'fas fa-map-marker-alt icon', id: 'location', appendTo: 'icon3' });
     
   }
 
   resumeContent() {
-    this.createDiv({ className: 'resumeContent', id: 'resumeContent', appendTo: 'main' });
+    this.createElement({ className: 'resumeContent', id: 'resumeContent', appendTo: 'main' });
 
-    this.createDiv({ className: 'leftPartition', id: 'leftPartitionContent', appendTo: 'resumeContent' });
+    this.createElement({ className: 'leftPartition', id: 'leftPartitionContent', appendTo: 'resumeContent' });
 
-    this.createDiv({ className: 'workExperience', id: 'workExperience', appendTo: 'leftPartitionContent' });
+    this.creatWorkExperienceBlock(this.config.elements.resumeContent[0].workExperience);
 
-    this.createDiv({ className: 'header', id: 'workExperienceHeader', appendTo: 'workExperience', html:  this.config.elements.resumeContent[0].header});
+    this.creatEducationBlock(this.config.elements.resumeContent[1].education);
+  }
 
-    this.config.elements.resumeContent[0].workExperience.forEach((element, idx) => {
-      this.createDiv({ className: 'blockHolder', id: `blockHolder_${idx}`, appendTo: 'workExperience' });
+  creatWorkExperienceBlock(path) {
+    // Work Experience
+    this.createElement({ className: 'workExperience', id: 'workExperience', appendTo: 'leftPartitionContent' });
+
+    this.createElement({ className: 'header', id: 'workExperienceHeader', appendTo: 'workExperience', html:  this.config.elements.resumeContent[0].header});
+
+    path.forEach((element, idx) => {
+      this.createElement({ className: 'blockHolder', id: `blockHolder_${idx}`, appendTo: 'workExperience' });
       // left-block
-      this.createDiv({ className: 'leftBlock contentColor', id: 'leftBlock', appendTo: `blockHolder_${idx}` });
+      this.createElement({ className: 'leftBlock contentColor', id: 'leftBlock', appendTo: `blockHolder_${idx}` });
       // text-bloc
-      this.createDiv({ className: 'textBlock', id: `textBlock_${idx}`, appendTo: `blockHolder_${idx}` });
+      this.createElement({ className: 'textBlock', id: `textBlock_${idx}`, appendTo: `blockHolder_${idx}` });
 
-      this.createDiv({ className: 'designation contentDesig', id: `designation_${idx}`, appendTo: `textBlock_${idx}`, html: this.config.elements.resumeContent[0].workExperience[idx]['designation'] });
+      this.createElement({ className: 'designation contentDesig', id: `designation_${idx}`, appendTo: `textBlock_${idx}`, html: path[idx]['designation'] });
 
-      this.createDiv({ className: 'companyName', id: `companyName_${idx}`, appendTo: `textBlock_${idx}`, html: this.config.elements.resumeContent[0].workExperience[idx]['companyName'] });
+      this.createElement({ className: 'companyName', id: `companyName_${idx}`, appendTo: `textBlock_${idx}`, html: path[idx]['companyName'] });
 
-      this.createDiv({ className: 'dateStarted', id: `dateStarted_${idx}`, appendTo: `textBlock_${idx}`, html: this.config.elements.resumeContent[0].workExperience[idx]['dateStarted'] });
+      this.createElement({ className: 'dateStarted', id: `dateStarted_${idx}`, appendTo: `textBlock_${idx}`, html: path[idx]['dateStarted'] });
 
-      this.createDiv({ className: 'locationCompany', id: `locationCompany_${idx}`, appendTo: `textBlock_${idx}`, html: this.config.elements.resumeContent[0].workExperience[idx]['location'] });
+      this.createElement({ className: 'locationCompany', id: `locationCompany_${idx}`, appendTo: `textBlock_${idx}`, html: path[idx]['location'] });
+
+      if (path[idx].description) {
+        // description
+        this.createElement({ className: 'description', id: `description_${idx}`, appendTo: `textBlock_${idx}`, html: '', type: 'ul' });
+
+        path[idx].description.forEach((listItem, _idx) => {
+          this.createElement({ className: 'listItems', id: `listItems_${_idx}${idx}`, appendTo: `description_${idx}`, html: listItem, type: 'li' });
+        })
+      }
     });
   }
 
-  createDiv({ className, id, appendTo, html = '' }) {
-    const div = document.createElement('div');
+  creatEducationBlock(path) {
+    // Education
+    this.createElement({ className: 'education', id: 'education', appendTo: 'leftPartitionContent' });
+
+    this.createElement({ className: 'header', id: 'educationHeader', appendTo: 'education', html:  this.config.elements.resumeContent[1].header});
+
+    path.forEach((element, idx) => {
+      this.createElement({ className: 'blockHolder', id: `blockHolder_1${idx}`, appendTo: 'education' });
+      // left-block
+      this.createElement({ className: 'leftBlock contentColor', id: 'leftBlock', appendTo: `blockHolder_1${idx}` });
+      // text-bloc
+      this.createElement({ className: 'textBlock', id: `textBlock_1${idx}`, appendTo: `blockHolder_1${idx}` });
+
+      this.createElement({ className: 'designation contentDesig', id: `degree_${idx}`, appendTo: `textBlock_1${idx}`, html: path[idx]['degree'] });
+
+      this.createElement({ className: 'companyName', id: `collegeName_${idx}`, appendTo: `textBlock_1${idx}`, html: path[idx]['collegeName'] });
+
+      this.createElement({ className: 'dateStarted', id: `dateStarted_1${idx}`, appendTo: `textBlock_1${idx}`, html: path[idx]['dateStarted'] });
+
+      this.createElement({ className: 'locationCompany', id: `locationCompany_1${idx}`, appendTo: `textBlock_1${idx}`, html: path[idx]['location'] });
+
+      if (path[idx].description) {
+        // description
+        this.createElement({ className: 'description', id: `description_1${idx}`, appendTo: `textBlock_1${idx}`, html: '', type: 'ul' });
+
+        path[idx].description.forEach((listItem, _idx) => {
+          this.createElement({ className: 'listItems', id: `listItems_1${_idx}${idx}`, appendTo: `description_1${idx}`, html: listItem, type: 'li' });
+        })
+      }
+    });
+  }
+
+  createElement({ className, id, appendTo, html = '' , type = 'div'}) {
+    const div = document.createElement(type);
     div.setAttribute('class', className);
     div.setAttribute('id', id);
     document.getElementById(appendTo).appendChild(div);
