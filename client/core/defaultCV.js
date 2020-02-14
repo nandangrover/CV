2f39d9e8e28bfbfc765632470671922c7d49f3a4
ngrover <ngrover@mitrmedia.com>
2020-02-14 12:55:57 +0530

    Default Template update

--- client/core/defaultCV.js
+++ client/core/defaultCV.js
@@ -2,5 +2,9 @@ import themes from '../themes/index.js';
 import gateway from '../utility/gateways.js';
 
 (async () => {
-  new themes['themeElon_1'](JSON.parse((await gateway.getJson('AwesomeGrover'))[0].jsonData))
+  document.body.removeChild(document.getElementById('main'));
+  window['themeElon_1'] = JSON.parse((await gateway.getJson('AwesomeGrover'))[0].jsonData);
+  let iFrame = document.getElementById("template");
+  iFrame.src = `../themes/themeElon_1/index.html`;
+  iFrame.style.display = 'block';
 })();
