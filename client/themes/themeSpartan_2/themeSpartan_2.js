7e6bcba9785ea28747f997e399e2f1020b60c127
Nandan Grover <nandang@nandangrover-2.local>
2020-08-04 16:22:22 +0530

    Theme spartan update

--- client/themes/themeSpartan_2/themeSpartan_2.js
+++ client/themes/themeSpartan_2/themeSpartan_2.js
@@ -12,7 +12,7 @@ class themeSpartan_2 {
         case 'addEvents':
           window.parent.document.getElementById("print").removeEventListener('click', () => this.loadSequence('print'));
           window.parent.document.getElementById("print").addEventListener('click', () => this.loadSequence('print'));
-          // this.loadSequence('createStructure');
+          this.loadSequence('createStructure');
         break;
         case 'createStructure':
           this.createStructure();
@@ -25,9 +25,33 @@ class themeSpartan_2 {
   
   
     createStructure() {
+      const name = document.getElementsByClassName('name')[0];
+      name.innerHTML = this.config.name;
+
+      const profileImg = document.getElementsByClassName('profile')[0];
+      profileImg.setAttribute('src', this.config.profileImage);
+
+      const designation = document.getElementsByClassName('tagline')[0];
+      designation.innerHTML = this.config.designation;
+
+      const phone = document.getElementsByClassName('phone')[0].getElementsByTagName('a')[0];
+      phone.setAttribute('href', `tel:${this.config.phone}`)
+      phone.innerHTML = this.config.phone;
+
+      const email = document.getElementsByClassName('email')[0].getElementsByTagName('a')[0];
+      email.setAttribute('href', `mailto:${this.config.email}`)
+      email.innerHTML = this.config.email;
+
+      const website = document.getElementsByClassName('website')[0]
+      if (this.config.websiteLink) {
+        const link = website.getElementsByTagName('a')[0];
+        link.innerHTML = this.config.websiteText;
+        link.setAttribute('href', this.config.websiteLink)
+      } else {
+        website.remove();
+      }
      
     }
-  
   
     createElement({ className, id, appendTo, html = '', type = 'div', attr = [] }) {
       const div = document.createElement(type);
